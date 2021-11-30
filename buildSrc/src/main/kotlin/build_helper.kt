93f43c6f829fc9e2207c2ad9367bea4b8035a516
Christoph Pickl <c.pickl@tbauctions.com>
2021-11-30 21:35:46 +0100

    move from docs to asciidoc

--- buildSrc/src/main/kotlin/build_helper.kt
+++ buildSrc/src/main/kotlin/build_helper.kt
@@ -3,7 +3,6 @@ import org.gradle.api.Project
 val Project.Locations get() = ProjectExtensions(this)
 
 class ProjectExtensions(private val project: Project) {
-    val sourceDirectory = project.file("docs")
+    val sourceDirectory = project.file("asciidoc")
     val htmlBuildOutput = project.file("${project.buildDir}/output/html")
 }
-
